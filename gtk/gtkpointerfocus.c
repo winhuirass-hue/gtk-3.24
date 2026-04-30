GNOME/gtk

commit dde6c95d2ac06deaf9b9f77741b0b6194a97e456

window: Avoid synthesizing crossing events early when unmapping a Popover
When unmapping a widget with pointerfocus, we want to repick a new focus target
and emit crossing events. However if the focused widget is part of a Popover
(or generally a GdkNative) that is being unmapped as a whole, then we know
that we will already emit crossing events later, when receiving surface
leave/enter events from the compositor.

Closes: https://gitlab.gnome.org/GNOME/gtk/-/issues/7432
Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/8351>

gtk/gtkwindow.c
@@ -6813,12 +6813,15 @@ gtk_window_update_pointer_focus_on_state_change (GtkWindow *window,
 
           old_target = g_object_ref (focus->target);
           gtk_pointer_focus_repick_target (focus);
-          gtk_synthesize_crossing_events (GTK_ROOT (window),
-                                          GTK_CROSSING_POINTER,
-                                          old_target, focus->target,
-                                          focus->x, focus->y,
-                                          GDK_CROSSING_NORMAL,
-                                          NULL);
+          if (gtk_widget_get_native (focus->target) == gtk_widget_get_native (old_target))
+            {
+              gtk_synthesize_crossing_events (GTK_ROOT (window),
+                                              GTK_CROSSING_POINTER,
+                                              old_target, focus->target,
+                                              focus->x, focus->y,
+                                              GDK_CROSSING_NORMAL,
+                                              NULL);
+            }
           g_object_unref (old_target);
         }
 

winhuirass-hue/gtk-3.24

commit 471a8834f5a5d182fd04049a6fd6f27915e5ec9a

window: Fixup pointer focus when unmapping a popover
When unmapping a popover we avoid synthesizing a widget crossing event until
receiving an Enter event from the compositor, but the Gtk.PointerFocus may
have already re-picked a new target widget (on a different native surface)
so the the next Enter event may find the same widget already focused and won't
emit a widget crossing event.

Before 50ca410700 ("surface: Ignore events for unmapped surface"), a compositor
Leave event from the already unmapped popover surface would reset the focus,
allowing the next Enter event to trigger new events. After that change, the
Leave event is dropped because it comes from an already unmapped surface.

We should reset the focus, synthesize a crossing event towards an NULL widget,
and eventually the next Enter event will trigger the crossing to a new widget.

Fixes: dde6c95d2a ("window: Avoid synthesizing crossing events early when unmapping a Popover")
Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9538>

gtk/gtkpointerfocus.c
@@ -130,13 +130,23 @@ gtk_pointer_focus_get_effective_target (GtkPointerFocus *focus)
   return target;
 }
 
-void
-gtk_pointer_focus_repick_target (GtkPointerFocus *focus)
+GtkWidget *
+gtk_pointer_focus_try_repick_target (GtkPointerFocus *focus)
 {
   GtkWidget *target;
 
   target = gtk_widget_pick (GTK_WIDGET (focus->toplevel), focus->x, focus->y, GTK_PICK_DEFAULT);
   if (target == NULL)
     target = GTK_WIDGET (focus->toplevel);
+
+  return target;
+}
+
+void
+gtk_pointer_focus_repick_target (GtkPointerFocus *focus)
+{
+  GtkWidget *target;
+
+  target = gtk_pointer_focus_try_repick_target (focus);
   gtk_pointer_focus_set_target (focus, target);
 }

gtk/gtkpointerfocusprivate.h
@@ -54,5 +54,6 @@ GtkWidget *       gtk_pointer_focus_get_implicit_grab (GtkPointerFocus *focus);
 
 GtkWidget *       gtk_pointer_focus_get_effective_target (GtkPointerFocus *focus);
 
+GtkWidget *       gtk_pointer_focus_try_repick_target (GtkPointerFocus *focus);
 void              gtk_pointer_focus_repick_target (GtkPointerFocus *focus);
 

gtk/gtkwindow.c
@@ -6813,18 +6813,28 @@ gtk_window_update_pointer_focus_on_state_change (GtkWindow *window,
                gtk_widget_is_ancestor (focus->target, widget))
         {
           GtkWidget *old_target;
+          GtkWidget *new_target;
 
           old_target = g_object_ref (focus->target);
-          gtk_pointer_focus_repick_target (focus);
-          if (gtk_widget_get_native (focus->target) == gtk_widget_get_native (old_target))
+          new_target = gtk_pointer_focus_try_repick_target (focus);
+          if (gtk_widget_get_native (new_target) == gtk_widget_get_native (old_target))
             {
-              gtk_synthesize_crossing_events (GTK_ROOT (window),
-                                              GTK_CROSSING_POINTER,
-                                              old_target, focus->target,
-                                              focus->x, focus->y,
-                                              GDK_CROSSING_NORMAL,
-                                              NULL);
+              gtk_pointer_focus_set_target (focus, new_target);
             }
+          else
+            {
+              new_target = NULL;
+              priv->foci = g_list_remove_link (priv->foci, l);
+              gtk_pointer_focus_unref (focus);
+              g_list_free (l);
+            }
+
+          gtk_synthesize_crossing_events (GTK_ROOT (window),
+                                          GTK_CROSSING_POINTER,
+                                          old_target, new_target,
+                                          focus->x, focus->y,
+                                          GDK_CROSSING_NORMAL,
+                                          NULL);
           g_object_unref (old_target);
         }
 