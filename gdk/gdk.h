c452b71a3376d35dd81658146a4fb559235be6da
winhuirass-hue <winhuirass@gmail.com>
2026-05-07 11:02:12 +0300

    Replace include guards with #pragma once

--- gdk/gdk.h
+++ gdk/gdk.h
@@ -22,8 +22,7 @@
  * GTK+ at ftp://ftp.gtk.org/pub/gtk/.
  */
 
-#ifndef __GDK_H__
-#define __GDK_H__
+#pragma once
 
 #define __GDK_H_INSIDE__
 
@@ -70,5 +69,3 @@
 #include <gdk/gdk-autocleanup.h>
 
 #undef __GDK_H_INSIDE__
-
-#endif /* __GDK_H__ */
